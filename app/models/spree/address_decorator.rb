d3294543ba8b2b90f47e34bd0eef66758e003de9
Jake Bell <jbbell@umn.edu>
2021-12-09 14:55:05 -0600

    Fixing issue with Shapco API when company or telephone are empty

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -2,17 +2,17 @@ Spree::Address.class_eval do
   def as_shapco
     {
       name: "#{firstname} #{lastname}",
-      company: company || 'None',
+      company: company.presence || 'None',
       street_address1: address1,
       street_address2: address2,
       city: city,
       postcode: zipcode,
       state: state.abbr,
       country: country.iso,
-      telephone: phone,
+      telephone: phone.presence || 'None',
       first_name: firstname,
       last_name: lastname,
-      corporate_name: company || 'None',
+      corporate_name: company.presence || 'None',
     }
   end
 end
